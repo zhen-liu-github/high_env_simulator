69e04356a0c8f9ce08d70aa3f1a721887f647e22
zhen.liu <zhen.liu@plus.ai>
2021-03-29 10:58:58 +0800

    solve_data_driven_method_bug

--- simulator/solver/data_driven_solver.py
+++ simulator/solver/data_driven_solver.py
@@ -7,33 +7,36 @@ import numpy as np
 import torch
 from .utils import LaneChangeWindow, CheckReady
 
+
 class DataDrivenSolver(BaseSolver):
     def __init__(self, model_config, constrains):
         super(DataDrivenSolver, self).__init__(model_config, constrains)
-        
+
         self._load_model()
 
-    def preprocess(self, observations):
-        return observations
+    def preprocess(self, obs_observations):
+        return obs_observations
+
     def postprocess(self, observation):
         window_list, best_index = observation
-        self.target_window =  self.window_list[best_index]
+        self.target_window = self.window_list[best_index]
         self.action['is_ready'] = False
-        if CheckReady(self.target_window, self.ego_car, env_config['ego_min_a'],
-                              env_config['vehicle_min_a']):
+        if CheckReady(self.target_window, self.ego_car,
+                      env_config['ego_min_a'], env_config['vehicle_min_a']):
             self.target_window.is_ready = True
             self.action['is_ready'] = True
-        self.GetControlOutput(self.ego_car[1], self.ego_car[3], self.target_window.window_s, self.target_window.window_v)
+        self.GetControlOutput(self.ego_car[1], self.ego_car[3],
+                              self.target_window.window_s,
+                              self.target_window.window_v)
         return self.action
 
-
-    def _solve(self, observations):
+    def _solve(self, obs_observations):
         # Get sample feature.
         # Need to improve the selected feature representation.
-        obs_list = observations[1:, 1:]
-        obs_list = [obs for obs in obs_list if obs[0]>0 and obs[2]>3 ]
+        obs_list = obs_observations[:, 1:]
+        obs_list = [obs for obs in obs_list if obs[0] > 0 and obs[1] > 3]
         feature = np.array(obs_list)
-        samples = np.zeros([feature.shape[0], feature.shape[1]*2])
+        samples = np.zeros([feature.shape[0], feature.shape[1] * 2])
         samples[:, :feature.shape[1]] = feature
         samples[:, feature.shape[1]:] = self.ego_car[1:]
         samples = torch.from_numpy(samples).float()
@@ -44,15 +47,20 @@ class DataDrivenSolver(BaseSolver):
         self.window_confidence = window_confidence.data.numpy()[0]
         self.best_window_index = np.argmax(self.window_confidence)
         self.window_list = []
+        target_obs_list = [
+            obs for obs in obs_observations if obs[0] > 0 and obs[2] > 3
+        ]
         for i in range(len(self.window_confidence)):
-            window = self.GetWindowByIndex(observations, i)
-            window.getWindowInfo(observations[0])
+            window = self.GetWindowByIndex(target_obs_list, i)
+            window.getWindowInfo(self.ego_car)
             self.window_list.append(window)
         return self.window_list, self.best_window_index
+
     def _load_model(self):
         self.model = torch.load(model_config['model_path'])
-    
-    def GetWindowByIndex(self, observation, index):
-        obs = observation[1:]
-        window = LaneChangeWindow(None if index==len(obs) else obs[index], None if index==0 else obs[index-1])
+
+    def GetWindowByIndex(self, obs_observation, index):
+        window = LaneChangeWindow(
+            None if index == len(obs_observation) else obs_observation[index],
+            None if index == 0 else obs_observation[index - 1])
         return window
--- simulator/solver/graphic.py
+++ simulator/solver/graphic.py
@@ -8,6 +8,8 @@ from rl_agents.utils import remap, constrain
 
 from simulator.config import model_config
 
+import pygame
+
 
 class SolverGraphic(object):
     """
@@ -27,7 +29,6 @@ class SolverGraphic(object):
         :param sim_surface: the pygame surface on which the env is rendered
         :param display_text: whether to display the window values as text
         """
-        import pygame
         target_window = solver.target_window
         front = (
             target_window.front_s - sim_surface.origin[0]
@@ -38,66 +39,56 @@ class SolverGraphic(object):
         cell_size = (front - rear, surface.get_height())
         pygame.draw.rect(surface, cls.BLACK,
                          (0, 0, surface.get_width(), surface.get_height()), 0)
+        window_rear_list = []
         if solver.model_config['type'] == 'data-driven' and solver.model_config[
                 'data-driven']['multi-window-display']:
+            # Draw all lane change window.
             for index, window in enumerate(solver.window_list):
-                window_front = (
-                    window.front_s - sim_surface.origin[0]
-                ) * sim_surface.scaling if window.front_is_valid else surface.get_width(
-                )
-                window_rear = (
-                    window.rear_s - sim_surface.origin[0]
-                ) * sim_surface.scaling if window.rear_is_valid else 0
-                cell_size = (window_front - window_rear, surface.get_height())
-                # Draw window
-                pygame.draw.rect(surface, cls.BLUE,
-                                 (window_rear, 0, cell_size[0], cell_size[1]),
-                                 0)
-                pygame.draw.rect(surface, cls.RED,
-                                 ((window.window_s - sim_surface.origin[0]) *
-                                  sim_surface.scaling - 2, 0, 4, cell_size[1]),
-                                 0)
-                # Display text
-                if display_text:
-                    font = pygame.font.Font(None, 15)
-                    text = "window s={:.2f}, v={:.2f}, w_f={:.2f}".format(
-                        window.window_s, solver.ego_car[3], solver.window_confidence[index])
-                    text = font.render(text, 2, (10, 10, 10), (255, 255, 255))
-                    surface.blit(text,
-                                 (rear + cell_size[0] / 2, cell_size[1] / 2))
-                # Display target window.
-            # Display node value
+                cls.DrawWindow(window, cls.BLUE, surface, sim_surface)
+            # Draw target window.
             cmap = cm.jet_r
             norm = mpl.colors.Normalize(vmin=0, vmax=1 / (1 - 0.5))
             color = cmap(norm(0.5), bytes=True)
-            pygame.draw.rect(surface, color, (rear, 0, cell_size[0], cell_size[1]),
-                            0)
-            pygame.draw.rect(surface, cls.RED,
-                            ((target_window.window_s - sim_surface.origin[0]) *
-                            sim_surface.scaling - 2, 0, 4, cell_size[1]), 0)
+            cls.DrawWindow(solver.target_window, color, surface, sim_surface)
+            # Display all window text
             if display_text:
-                font = pygame.font.Font(None, 30)
-                text = "a={:.2f}, w_f={:.2f}, ego_v:{:.2f}, r_v={:.2f} r_s:{:.2f}".format(solver.action['a'],
-                    solver.window_confidence[solver.best_window_index], solver.ego_car[3], target_window.window_v - solver.ego_car[3],
-                    target_window.window_s - solver.ego_car[1])
-                text = font.render(text, 1, (10, 10, 10), (255, 255, 255))
-                surface.blit(text, (rear + cell_size[0] / 2, cell_size[1] / 2))
+                for index, window in enumerate(solver.window_list):
+                    cls.DrawText(
+                        window,
+                        'window_s={:.2f}, window_index={}, window_v={:.2f}, w_f:{:.2f}'.format(
+                            window.window_s, index, window.window_v,
+                            solver.window_confidence[index]),
+                        surface.get_height() / len(solver.window_list) * index,
+                        surface, sim_surface)
+
+                # Display target window text.
+                cls.DrawText(
+                    solver.target_window,
+                    'window_s={:.2f}, window_v={:.2f}, w_f:{:.2f}'.format(
+                        solver.target_window.window_s,
+                        solver.target_window.window_v,
+                        solver.window_confidence[solver.best_window_index],
+                    ),
+                    surface.get_height() / 2, surface, sim_surface)
+
         else:
-            
+
             best_time = solver.best_time
             # Display node value
             cmap = cm.jet_r
             norm = mpl.colors.Normalize(vmin=0, vmax=1 / (1 - 0.5))
             color = cmap(norm(0.5), bytes=True)
-            pygame.draw.rect(surface, color, (rear, 0, cell_size[0], cell_size[1]),
-                            0)
-            pygame.draw.rect(surface, cls.RED,
-                            ((target_window.window_s - sim_surface.origin[0]) *
-                            sim_surface.scaling - 2, 0, 4, cell_size[1]), 0)
+            pygame.draw.rect(surface, color,
+                             (rear, 0, cell_size[0], cell_size[1]), 0)
+            pygame.draw.rect(
+                surface, cls.RED,
+                ((target_window.window_s - sim_surface.origin[0]) *
+                 sim_surface.scaling - 2, 0, 4, cell_size[1]), 0)
             if display_text:
                 font = pygame.font.Font(None, 30)
-                text = "a={:.2f}, time={:.2f}, ego_v:{:.2f}, r_v={:.2f} r_s:{:.2f}".format(solver.action['a'],
-                    best_time, solver.ego_car[3], target_window.window_v - solver.ego_car[3],
+                text = "a={:.2f}, time={:.2f}, ego_v:{:.2f}, r_v={:.2f} r_s:{:.2f}".format(
+                    solver.action['a'], best_time, solver.ego_car[3],
+                    target_window.window_v - solver.ego_car[3],
                     target_window.window_s - solver.ego_car[1])
                 text = font.render(text, 1, (10, 10, 10), (255, 255, 255))
                 surface.blit(text, (rear + cell_size[0] / 2, cell_size[1] / 2))
@@ -110,3 +101,28 @@ class SolverGraphic(object):
             return cls.display(solver, surface, sim_surface, display_text)
 
         return inner
+
+    @classmethod
+    def DrawWindow(cls, window, color, surface, sim_surface):
+        window_front = (
+            window.front_s - sim_surface.origin[0]
+        ) * sim_surface.scaling if window.front_is_valid else surface.get_width(
+        )
+        window_rear = (window.rear_s - sim_surface.origin[0]
+                       ) * sim_surface.scaling if window.rear_is_valid else 0
+        cell_size = (window_front - window_rear, surface.get_height())
+        pygame.draw.rect(surface, color,
+                         (window_rear, 0, cell_size[0], cell_size[1]), 0)
+        pygame.draw.rect(
+            surface, cls.RED,
+            ((window.window_s - sim_surface.origin[0]) * sim_surface.scaling -
+             2, 0, 4, cell_size[1]), 0)
+
+    @classmethod
+    def DrawText(cls, window, text, pos, surface, sim_surface):
+        font = pygame.font.Font(None, 20)
+        text = font.render(text, 1, (10, 10, 10), (255, 255, 255))
+        window_rear = (window.rear_s - sim_surface.origin[0]
+                       ) * sim_surface.scaling if window.rear_is_valid else 0
+
+        surface.blit(text, [window_rear + 5, pos])
